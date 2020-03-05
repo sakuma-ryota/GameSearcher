e9b7da4792d6c3e697ddd3efcb7481fc9e8ce8cb
”sakuma” <srff7cloud@yahoo.co.jp>
2020-03-05 17:06:28 +0900

    あ

---
+++ resources/views/user/game/index.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <title>アプリ一覧</title>
+</head>
+<body>
+    <h1>アプリ一覧</h1>
+</body>
+</html>
