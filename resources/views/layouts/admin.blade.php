c4b4448bc0be2364e56408e25dda60a7a1fd85d6
”sakuma” <srff7cloud@yahoo.co.jp>
2020-03-07 12:51:14 +0900

    adminのナビゲーション部分の作成 #2

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -9,21 +9,37 @@
     <title>@yield('title')</title>
 
     <!-- Laravelのjavascripiptの読み込み -->
-    <script src="{{ secure_asset('js/app.js') }}" defer></script>
+    <script src="{{ secure_asset('js/app.js')}}" defer></script>
 
     <!-- Fonts -->
     <link rel="dns-prefetch" href="https://fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css?family=Raleway:300,400,600" rel="stylesheet" type="text/css">
 
     <!-- laravelのCSSの読み込み -->
-    <link href="{{ secure_asset('css/app.css') }}" rel="stylesheet">
+    <link href="{{ secure_asset('css/app.css') }}" rel ="stylesheet">
 
     <!-- 自作のCSSの読み込み -->
-    <link href="{{ secure_asset('css/admin.css') }}" rel ="stylesheet">
+    <link href="{{ secure_asset('css/admin.css')}}" rel ="stylesheet">
 
 </head>
 <body>
- 
+    <div id="app">
+        <nav class="navbar navbar-expand-md navbar-dark navbar-laravel">
+            <div class="container">
+                <a class="navbar-brand" href="{{ url('/') }}">
+                    {{ config('app.name', 'Laravel') }}
+                </a>
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul class="navbar-nav mr-auto">
+                    </ul>
+                    <ul class="navbar-nav ml-auto">
+                    </ul>
+                </div>
+            </div>    
+        </nav>
         <div class="container">
             <div class="row">
                 <div class="col-md-8 mx-auto">
