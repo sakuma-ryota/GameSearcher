520eb15f75abc382888a69e5914bf9abf2388810
”sakuma” <srff7cloud@yahoo.co.jp>
2020-03-08 13:04:42 +0900

    データベースの作成 #4

---
+++ database/migrations/2020_03_07_091842_create_games_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateGamesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('games', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('image_path');
+            $table->string('title');
+            $table->string('day');
+            $table->string('genre');
+            $table->string('applink');
+            $table->string('googlelink');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('games');
+    }
+}
