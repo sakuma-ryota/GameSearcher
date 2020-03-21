ef6703be8a8e9d7fc2aacd18614c82418ad35139
”sakuma” <srff7cloud@yahoo.co.jp>
2020-03-21 14:11:22 +0900

    変更

--- resources/views/admin/game/index.blade.php
+++ resources/views/admin/game/index.blade.php
@@ -53,7 +53,7 @@
                                     <td>{{ $game->id }}</td>
                                     <td>
                                         @if ($game->image_path)
-                                            <img src="{{ $game->image_path) }}" class="index-icon">
+                                            <img src="{{ $game->image_path }}" class="index-icon">
                                         @endif
                                     </td>
                                     <td>{!! nl2br(e($game->title)) !!}</td>
